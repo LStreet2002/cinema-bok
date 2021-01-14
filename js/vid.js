d92cc1c9d6971cb6471cade04f3d777b9c6ddd1d
Lewis Street <lstreet@salisbury6c.ac.uk>
2021-01-14 16:03:12 +0000

    style menu

--- js/vid.js
+++ js/vid.js
@@ -9,3 +9,15 @@ firebase.auth().onAuthStateChanged(function (user) {
 function back() {
     window.location.replace("menu.html")
 }
+function sage(e) {
+    var rese = document.querySelectorAll(".agi")
+    console.log(rese)
+    for (i = 0; i < rese.length; i++) {
+        rese[i].style.backgroundColor = "rgba(0,0,0,0)"
+        rese[i].style.color = "rgb(0,0,0)"
+        rese[i].setAttribute("name", "")
+    }
+    e.style.backgroundColor = "#0A0903"
+    e.style.color = "white"
+    e.setAttribute("name", "active")
+}
