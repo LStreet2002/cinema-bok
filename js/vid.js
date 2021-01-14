dfbd5b1f7eeaef96bc03d1110cdbe406ad0055aa
Lewis Street <lstreet@salisbury6c.ac.uk>
2021-01-14 19:56:37 +0000

    add movie

--- js/vid.js
+++ js/vid.js
@@ -11,7 +11,6 @@ function back() {
 }
 function sage(e) {
     var rese = document.querySelectorAll(".agi")
-    console.log(rese)
     for (i = 0; i < rese.length; i++) {
         rese[i].style.backgroundColor = "rgba(0,0,0,0)"
         rese[i].style.color = "rgb(0,0,0)"
@@ -20,4 +19,40 @@ function sage(e) {
     e.style.backgroundColor = "#0A0903"
     e.style.color = "white"
     e.setAttribute("name", "active")
+}
+var chooser = document.querySelector("#pseu")
+chooser.addEventListener("change", function (e) {
+    // Get file
+    file = e.target.files[0];
+
+    console.log(file.name);
+
+    output = document.getElementById("trai");
+    var tempurl = URL.createObjectURL(event.target.files[0]);
+    output.src = tempurl
+    tempimage = tempurl
+    output.onload = function () { // free memory
+    }
+});
+function chose() {
+    chooser.click()
+}
+async function plu() {
+
+    db.collection("movies").doc(document.querySelector("#title").querySelector(".bawx").value)
+        .set({
+            title: document.querySelector("#title").querySelector(".bawx").value,
+            length: document.querySelector("#length").querySelector(".bawx").value,
+            description: document.querySelector("#descrip").querySelector(".bawx").value,
+            additional: document.querySelector("#addit").querySelector(".bawx").value,
+            age: document.getElementsByName("active")[0].innerText,
+            trailer: file.name
+        }).then(function (docRef) {
+            var storageRef = firebase.storage().ref("trailers/" + file.name);
+            // Upload file
+            var task = storageRef.put(file);;
+
+        }).then(function () {
+            location.reload()
+        })
 }
