704e17c30fa88e74b0bdcfc0397df8474494f1cf
Lewis Street <lstreet@salisbury6c.ac.uk>
2021-01-14 12:31:14 +0000

    videos

--- js/login.js
+++ js/login.js
@@ -5,33 +5,37 @@ document.querySelector("#admin-box").addEventListener("keyup", function (event) 
     }
 });
 function login(e) {
-    var docRef = db.collection("users").doc(e.parentNode.querySelector("#Emil").value);
-    docRef.get().then(function (doc) {
-        if (doc.exists) {
-            console.log(doc.data().type)
-            if (doc.data().type === "admin") {
-                (function () {
-                    console.log("here")
-                    const logins = document.querySelector("#admin-box");
+    firebase
+        .auth()
+        .setPersistence(firebase.auth.Auth.Persistence.SESSION)
+        .then(function () {
+            var docRef = db.collection("users").doc(e.parentNode.querySelector("#Emil").value);
+            docRef.get().then(function (doc) {
+                if (doc.exists) {
+                    console.log(doc.data().type)
+                    if (doc.data().type === "admin") {
+                        (function () {
+                            console.log("here")
+                            const logins = document.querySelector("#admin-box");
 
 
-                    const email = logins["Emil"].value;
-                    const password = logins["passowrd"].value;
+                            const email = logins["Emil"].value;
+                            const password = logins["passowrd"].value;
 
-                    // firebase login
-                    auth.signInWithEmailAndPassword(email, password).then((cred) => {
-                        window.location.replace("menu.html");
-                    }).catch(error => document.querySelector("#conform").style.backgroundColor = "red",
-                        setTimeout(function () { document.querySelector("#conform").style.backgroundColor = " #0A0903"; }, 15000))
+                            // firebase login
+                            auth.signInWithEmailAndPassword(email, password).then((cred) => {
+                                window.location.replace("menu.html");
+                            }).catch(error => document.querySelector("#conform").style.backgroundColor = "red",
+                                setTimeout(function () { document.querySelector("#conform").style.backgroundColor = " #0A0903"; }, 15000))
 
-                })();
-            }
-        } else {
-            // doc.data() will be undefined in this case
-            console.log("No such document!");
-        }
-    }).catch(function (error) {
-        console.log("Error getting document:", error);
-    });
-
+                        })();
+                    }
+                } else {
+                    // doc.data() will be undefined in this case
+                    console.log("No such document!");
+                }
+            }).catch(function (error) {
+                console.log("Error getting document:", error);
+            });
+        })
 }
--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,18 @@
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+    }
+    else {
+        window.location.replace("login.html")
+    }
+})
+// User is signed in.
 function back() {
     window.location.replace("login.html")
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+    }).catch((error) => {
+        // An error happened.
+    });
 }
 function tab(e) {
     window.location.replace(e.parentNode.id + ".html")
