c9c737f0fde3b6410a5d3925c8f209918c1679b2
Lewis Street <lstreet@salisbury6c.ac.uk>
2021-01-15 18:39:01 +0000

    edit page

--- js/lis.js
+++ js/lis.js
@@ -1,7 +1,13 @@
 
 
 function back() {
-    window.location.replace("menu.html")
+    if (document.querySelector("#form").style.display === "grid") {
+        document.querySelector("#form").style.display = "none"
+        document.querySelector("#helder").style.display = "grid"
+    }
+    else {
+        window.location.replace("menu.html")
+    }
 }
 firebase.auth().onAuthStateChanged(async function (user) {
     if (user) {
@@ -17,14 +23,17 @@ firebase.auth().onAuthStateChanged(async function (user) {
                         .getDownloadURL()
                         .then(function (url) {
                             var pocket = document.createElement("div")
+                            pocket.setAttribute("onclick", "edit(this)")
                             pocket.classList.add("pocket")
 
                             var trai = document.createElement("video");
+                            trai.setAttribute("name", doc.data().trailer)
                             trai.classList.add("prev");
                             trai.src = url;
 
                             var age = document.createElement("img");
                             age.classList.add("agg");
+                            age.setAttribute("name", doc.data().age)
                             age.src = "pic/" + doc.data().age + ".png";
 
                             var litle = document.createElement("p")
@@ -33,17 +42,22 @@ firebase.auth().onAuthStateChanged(async function (user) {
 
                             var time = document.createElement("p")
                             time.classList.add("time")
-                            time.innerText = doc.data().length
+                            time.innerText = doc.data().length + " minutes"
 
                             var dess = document.createElement("p")
                             dess.classList.add("dess")
                             dess.innerText = doc.data().description
+
+                            var adie = document.createElement("p")
+                            adie.classList.add("adie")
+                            adie.innerText = doc.data().additional
 
                             pocket.appendChild(trai)
                             pocket.appendChild(litle)
                             pocket.appendChild(time)
                             pocket.appendChild(age)
                             pocket.appendChild(dess)
+                            pocket.appendChild(adie)
 
                             document.querySelector("#helder").appendChild(pocket)
 
@@ -56,3 +70,52 @@ firebase.auth().onAuthStateChanged(async function (user) {
         window.location.replace("login.html")
     }
 })
+function edit(e) {
+    document.querySelector("#form").style.display = "grid"
+    document.querySelector("#helder").style.display = "none"
+    document.querySelector("#trai").src = e.querySelector(".prev").src
+    document.querySelector("#trai").setAttribute("name", e.querySelector(".prev").getAttribute("name"))
+    document.querySelector("#title").querySelector(".pbawx").innerText = e.querySelector(".litle").innerText
+    document.querySelector("#length").querySelector(".bawx").value = e.querySelector(".time").innerText
+    document.querySelector("#descrip").querySelector(".bawx").value = e.querySelector(".dess").innerText
+    document.querySelector("#addit").querySelector(".bawx").value = e.querySelector(".adie").innerText
+    var rese = document.querySelectorAll(".agi")
+    for (i = 0; i < rese.length; i++) {
+        if (rese[i].innerText === e.querySelector(".agg").name) {
+            rese[i].style.backgroundColor = "#0A0903"
+            rese[i].style.color = "white"
+            rese[i].setAttribute("name", "active")
+        }
+        else {
+            rese[i].style.backgroundColor = "rgba(0,0,0,0)"
+            rese[i].style.color = "#000000"
+            rese[i].setAttribute("name", "")
+        }
+    }
+}
+function sage(e) {
+    var rese = document.querySelectorAll(".agi")
+    for (i = 0; i < rese.length; i++) {
+        rese[i].style.backgroundColor = "rgba(0,0,0,0)"
+        rese[i].style.color = "rgb(0,0,0)"
+        rese[i].setAttribute("name", "")
+    }
+    e.style.backgroundColor = "#0A0903"
+    e.style.color = "white"
+    e.setAttribute("name", "active")
+}
+var chooser = document.querySelector("#pseu")
+chooser.addEventListener("change", function (e) {
+    // Get file
+    file = e.target.files[0];
+
+    output = document.getElementById("trai");
+    var tempurl = URL.createObjectURL(event.target.files[0]);
+    output.src = tempurl
+    tempimage = tempurl
+    output.onload = function () { // free memory
+    }
+});
+function chose() {
+    chooser.click()
+}
