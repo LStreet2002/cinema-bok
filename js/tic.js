84405b803e9f3ea524c22aaad01286a89223d3ab
Lewis Street <lstreet@salisbury6c.ac.uk>
2021-01-23 16:25:52 +0000

    many loops

--- js/tic.js
+++ js/tic.js
@@ -3,13 +3,18 @@ firebase.auth().onAuthStateChanged(async function (user) {
         var roms = ["A", "B", "C", "D", "E", "F"]
         for (var i = 0; i < roms.length; i++) {
             await db
+                .collection(roms[i]).doc("Monday").get().then(function (doc) {
+                    document.querySelector("#" + roms[i]).querySelector(".screen").innerText = doc.data().nine
+                })
+            await db
                 .collection(roms[i]).where("type", "==", "seat")
                 .get()
                 .then(async function (querySnapshot) {
                     querySnapshot.forEach(async function (doc) {
                         var conta = document.createElement("img")
                         conta.classList.add("conta")
-                        if (doc.data().status === "booked") {
+                        conta.id = doc.id
+                        if (doc.data().mondaynine === "booked") {
                             conta.src = "/pic/book.png"
                         }
                         else {
@@ -20,6 +25,21 @@ firebase.auth().onAuthStateChanged(async function (user) {
                     })
                 })
         }
+        await db
+            .collection("movies")
+            .get()
+            .then(async function (querySnapshot) {
+                var boses = document.querySelectorAll(".filmselec")
+                for (var i = 0; i < boses.length; i++) {
+                    querySnapshot.forEach(async function (doc) {
+                        var opto = document.createElement("option")
+                        opto.value = doc.data().title
+                        opto.innerText = doc.data().title
+                        boses[i].appendChild(opto)
+
+                    })
+                }
+            })
     }
     else {
         window.location.replace("login.html")
@@ -29,7 +49,7 @@ var first = document.querySelectorAll(".roomb")
 for (var i = 0; i < first.length; i++) {
     first[i].addEventListener("click", function (event) {
         document.querySelector("#subroom").style.display = "block"
-        document.querySelector("#" + event.target.innerText).style.display = "block"
+        document.querySelector("#" + event.target.innerText).style.display = "grid"
         event.target.parentNode.style.display = "none"
     });
 }
@@ -48,7 +68,7 @@ function back() {
         }
         else {
             document.querySelector("#times").style.display = "none"
-            document.querySelector("#" + document.querySelector("#ident").innerText).style.display = "block"
+            document.querySelector("#" + document.querySelector("#ident").innerText).style.display = "grid"
             document.querySelector("#subroom").style.display = "block"
         }
     }
@@ -61,15 +81,32 @@ for (var i = 0; i < timmy.length; i++) {
         document.querySelector("#subroom").style.display = "none"
         event.target.parentNode.style.display = "none"
         document.querySelector("#ident").innerText = event.target.parentNode.id
+
     });
 }
 
 async function movvie(e) {
     test = String(e.value)
-    e.setAttribute("name", e.options[e.selectedIndex].getAttribute("name"))
-    await db.collection(e.parentNode.id).doc(e.name).get().then(function (doc) {
+    dae = e.options[e.selectedIndex].getAttribute("name")
+    tillee = dae + test
+    e.setAttribute("name", dae)
+    await db.collection(e.parentNode.id).doc(e.name).get().then(async function (doc) {
         if (doc.exists) {
             e.parentNode.querySelector(".screen").innerText = doc.data()[test];
+            await db
+                .collection(e.parentNode.id).where("type", "==", "seat")
+                .get()
+                .then(async function (querySnapshot) {
+                    querySnapshot.forEach(async function (doc) {
+                        var reconta = document.querySelector("#" + doc.id)
+                        if (doc.data()[tillee] === "booked") {
+                            reconta.src = "/pic/book.png"
+                        }
+                        else {
+                            reconta.src = "/pic/unbook.png"
+                        }
+                    })
+                })
         }
     })
 }
