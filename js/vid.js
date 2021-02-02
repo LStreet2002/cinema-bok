8e3997faa7109d5770e9f0b46355ec279f037c48
Lewis Street <lstreet@salisbury6c.ac.uk>
2021-02-02 08:51:30 +0000

    escape

--- js/vid.js
+++ js/vid.js
@@ -38,6 +38,61 @@ function chose() {
     chooser.click()
 }
 async function plu() {
+    if (document.querySelector("#title").querySelector(".bawx").value === "") {
+        document.querySelector("#add").innerText = "NO TITLE"
+        document.querySelector("#add").style.backgroundColor = "red"
+        setTimeout(function () { document.querySelector("#add").style.backgroundColor = " #0A0903"; document.querySelector("#add").innerText = "ADD" }, 1500)
+        return
+    }
+    else {
+        if (document.querySelector("#length").querySelector(".bawx").value === "") {
+            document.querySelector("#add").innerText = "NO TIME"
+            document.querySelector("#add").style.backgroundColor = "red"
+            setTimeout(function () { document.querySelector("#add").style.backgroundColor = " #0A0903"; document.querySelector("#add").innerText = "ADD" }, 1500)
+            return
+        }
+        else {
+            if (document.querySelector("#descrip").querySelector(".bawx").value === "") {
+                document.querySelector("#add").innerText = "NO DESC"
+                document.querySelector("#add").style.backgroundColor = "red"
+                setTimeout(function () { document.querySelector("#add").style.backgroundColor = " #0A0903"; document.querySelector("#add").innerText = "ADD" }, 1500)
+                return
+            }
+            else {
+                if (document.querySelector("#addit").querySelector(".bawx").value === "") {
+                    document.querySelector("#add").innerText = "NO ADDITONAL"
+                    document.querySelector("#add").style.backgroundColor = "red"
+                    setTimeout(function () { document.querySelector("#add").style.backgroundColor = " #0A0903"; document.querySelector("#add").innerText = "ADD" }, 1500)
+                    return
+                }
+                else {
+                    if (document.getElementsByName("active")[0] == null) {
+                        document.querySelector("#add").innerText = "NO AGE"
+                        document.querySelector("#add").style.backgroundColor = "red"
+                        setTimeout(function () { document.querySelector("#add").style.backgroundColor = " #0A0903"; document.querySelector("#add").innerText = "ADD" }, 1500)
+                        return
+                    }
+                    else {
+                        if (document.getElementById("gen").value === "") {
+                            document.querySelector("#add").innerText = "NO GENRE"
+                            document.querySelector("#add").style.backgroundColor = "red"
+                            setTimeout(function () { document.querySelector("#add").style.backgroundColor = " #0A0903"; document.querySelector("#add").innerText = "ADD" }, 1500)
+                            return
+                        }
+                        else {
+                            if (document.querySelector("#pseu").value === "") {
+                                document.querySelector("#add").innerText = "NO TRAILER"
+                                document.querySelector("#add").style.backgroundColor = "red"
+                                setTimeout(function () { document.querySelector("#add").style.backgroundColor = " #0A0903"; document.querySelector("#add").innerText = "ADD" }, 1500)
+                                return
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
 
     await db.collection("movies").doc(document.querySelector("#title").querySelector(".bawx").value)
         .set({
