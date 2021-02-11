a798c7d04487218d7aa50ac7c11bfd317fd79f2b
Lewis Street <lstreet@salisbury6c.ac.uk>
2021-02-11 12:31:26 +0000

    genupdate

--- js/vid.js
+++ js/vid.js
@@ -5,9 +5,83 @@ firebase.auth().onAuthStateChanged(function (user) {
         window.location.replace("login.html")
     }
 })
+window.addEventListener("DOMContentLoaded", async function () {
+    await db.collection("genres").doc("genrez").get().then((doc) => {
+        if (doc.exists) {
+            var genks = doc.data().genre
+            for (var i = 0; i < genks.length; i++) {
+                var inpu = document.createElement("input")
+                inpu.value = genks[i]
+                inpu.classList.add("inpew")
+                var divop = document.createElement("div")
+                divop.classList.add("divop")
+                var minus = document.createElement("div")
+                minus.innerText = "Remove"
+                minus.setAttribute("onclick", "removet(this)")
+                minus.classList.add("minus")
+                divop.appendChild(inpu)
+                divop.appendChild(minus)
+                var opso = document.createElement("option")
+                opso.classList.add("opsos")
+                opso.setAttribute("value", genks[i])
+                opso.innerText = genks[[i]]
+                document.querySelector("#gen").appendChild(opso)
+                document.querySelector("#genrebox").appendChild(divop)
+            }
+        }
+    }).then(function () {
+        var plu = document.createElement("img")
+        plu.id = "plux"
+        plu.src = "pic/plus.png"
+        plu.classList.add("hovv")
+        plu.setAttribute("onclick", "plus(this)")
+        document.querySelector("#genrebox").appendChild(plu)
+        var add = document.createElement("div")
+        add.classList.add("hovv")
+        add.innerText = "UPDATE"
+        add.id = "addex"
+        add.setAttribute("onclick", "genupt()")
+        document.querySelector("#genrebox").appendChild(add)
+
+    })
+})
+function plus(e) {
+    document.querySelector("#plux").remove()
+    document.querySelector("#addex").remove()
+    var plu = document.createElement("img")
+    plu.id = "plux"
+    plu.src = "pic/plus.png"
+    plu.classList.add("hovv")
+    plu.setAttribute("onclick", "plus(this)")
+    var inpu = document.createElement("input")
+    inpu.value = ""
+    inpu.classList.add("inpew")
+    var divop = document.createElement("div")
+    divop.classList.add("divop")
+    var minus = document.createElement("div")
+    minus.innerText = "Remove"
+    minus.setAttribute("onclick", "removet(this)")
+    minus.classList.add("minus")
+    divop.appendChild(inpu)
+    divop.appendChild(minus)
+    document.querySelector("#genrebox").appendChild(divop)
+    document.querySelector("#genrebox").appendChild(plu)
+    var add = document.createElement("div")
+    add.classList.add("hovv")
+    add.innerText = "UPDATE"
+    add.id = "addex"
+    add.setAttribute("onclick", "genupt()")
+    document.querySelector("#genrebox").appendChild(add)
+}
 
 function back() {
-    window.location.replace("menu.html")
+    if (document.querySelector("#form").style.display === "grid") {
+        window.location.replace("menu.html")
+    }
+    else {
+        document.querySelector("#genrebox").style.display = "none"
+        document.querySelector("#form").style.display = "grid"
+    }
 }
 function sage(e) {
     var rese = document.querySelectorAll(".agi")
@@ -139,4 +213,38 @@ async function plu() {
         }).then(function () {
             location.reload()
         })
+}
+function genrepag() {
+    document.querySelector("#form").style.display = "none"
+    document.querySelector("#genrebox").style.display = "block"
+}
+function genupt() {
+    var genres = document.querySelectorAll(".inpew")
+    var sarr = []
+    for (var l = 0; l < genres.length; l++) {
+        sarr.push(genres[l].value)
+    }
+    db.collection("genres").doc("genrez").update({
+        genre: sarr
+    })
+    db.collection("genres").doc("genrez").get().then((doc) => {
+        if (doc.exists) {
+            var genks = doc.data().genre
+            var opple = document.querySelectorAll(".opsos")
+            console.log(opple)
+            for (var w = 0; w < opple.length; w++) {
+                opple[w].remove()
+            }
+            for (var i = 0; i < genks.length; i++) {
+                var opso = document.createElement("option")
+                opso.setAttribute("value", genks[i])
+                opso.classList.add("opsos")
+                opso.innerText = genks[[i]]
+                document.querySelector("#gen").appendChild(opso)
+            }
+        }
+    })
+}
+function removet(e) {
+    e.parentNode.remove()
 }
